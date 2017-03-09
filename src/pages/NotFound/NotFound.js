4b5da0cef032385c9d6968ddb7b2bc92d8a8a260
shuvohabib <habib.islam@gagagugu.com>
2017-03-10 05:04:28 +0600

    Add react-router-boilerplate

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div className="Contact">
+                <h1>Not Found</h1>
+            </div>
+        );
+    }
+}
+export default NotFound;
